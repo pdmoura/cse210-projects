a7d51e52fb049bcf45b1074008863df892a4c5be
pdmoura <sudwarriormoura@gmail.com>
2025-03-08 12:01:10 -0300

    Added the stretch challenges to ask the user to play again and a counter for the number of guesses the user made to be displayed at the end of the program

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 class Program
 {
@@ -6,27 +7,43 @@ class Program
     {
         // Console.Write("What is the magic number? "); when the user was choosing the magic number
         // int magicNumber = int.Parse(Console.ReadLine());
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
         int userGuess = -1;
+        string response = "yes";
+        int numberOfGuesses = 1;
+        do
+        {   
+            // Initialize the random number 
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            Console.WriteLine(magicNumber);
+            // Start the guessing game
+            while(userGuess != magicNumber)
+            {
+                Console.Write("Guess the magic number: ");
+                userGuess = int.Parse(Console.ReadLine());
 
-        while(userGuess != magicNumber)
-        {
-            Console.Write("Guess the magic number: ");
-            userGuess = int.Parse(Console.ReadLine());
-
-            if (userGuess > magicNumber)
-            {
-                Console.WriteLine("Lower");
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    numberOfGuesses += 1;
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    numberOfGuesses += 1;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numberOfGuesses} attempts.");
+                    //Restart the counter to prepare if the user wants to play again
+                    numberOfGuesses = 1; 
+                    // Ask if the user wants to play again
+                    Console.Write("Do you want to play again? (yes or no) ");
+                    response = Console.ReadLine().ToLower();
+                }
             }
-            else if (userGuess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }
+        }while(response=="yes");
+        Console.WriteLine("Thank you, have a great day!");
     }
 }
