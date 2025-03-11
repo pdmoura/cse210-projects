18b563569e670f9d59c93d6015ae93d6440c50b8
pdmoura <sudwarriormoura@gmail.com>
2025-03-11 20:00:04 -0300

    changed the value of the attributes to match the example

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,16 +6,16 @@ class Program
     {
         // Instantiating the classes and assigning values to the attributes
         Job job1 = new Job();
-        job1._company = "Provide Media";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 2024;
-        job1._endYear = 2026;
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
-        job2._company = "Bloom";
-        job2._jobTitle = "Web Frontend Developer";
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
         job2._startYear = 2022;
-        job2._endYear = 2029;
+        job2._endYear = 2023;
 
         Resume myResume = new Resume();
         myResume._personName = "Pedro Alves";
