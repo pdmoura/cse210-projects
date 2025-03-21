6728bcbf7d0460802e52e76c1f3785614fe977df
pdmoura <sudwarriormoura@gmail.com>
2025-03-21 17:04:15 -0300

    Finished the Scripture memorizer project adding a stretch to allow the user to show words that were hidden by typing show, if no word was hidden yet it will ask the user to press any key and start again following the loop till the user type quit.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+//As a way to exceed requirements I added the option for the user to show some words that were hidden by typing show.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        //Using the second constructor of the Reference class
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide more words, type 'show' to reveal some hidden words, or type 'quit' to exit.");
+            string input = Console.ReadLine().Trim().ToLower();
+            if (input == "quit")
+            {
+                break;
+            }
+
+            else if (input == "show")
+            {
+                scripture.ShowRandomWords(2); // Show 2 random hidden words
+            }
+
+            else
+            {
+                scripture.HideRandomWords(2);
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
     }
 }
+
+
+
+
