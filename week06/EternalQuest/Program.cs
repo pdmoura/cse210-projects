8b874ff8c35a6ed32ef09e7c2764943b0d95abdf
pdmoura <sudwarriormoura@gmail.com>
2025-04-12 15:36:15 -0300

    Add a comment explaining how I exceeded the requirements

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-
+//As a way to exceed requirements I added a function in the Goal Class named ShowCelebration using 
+//some ASCII art, and added some phrases and a few Console.Clear() lines to make the transitions more readable throughout the program
 
 class Program
 {
