83242a176bf529b4e4d2500dc57dd6b16d50df8b
pdmoura <sudwarriormoura@gmail.com>
2025-04-15 03:20:33 -0300

    Finished the main code but still need to fix some minor issues with the calculations and float points

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        string date = DateTime.Now.ToString("dd MMM yyyy", new CultureInfo("en-US"));
+        Running runningActivity = new Running(date, 30, 4.8);
+        Console.WriteLine(runningActivity.GetSummary());
     }
 }
